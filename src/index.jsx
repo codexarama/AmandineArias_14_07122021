e72fdfe9bb316fafae9a42c5af3bceebeac714c3
Amandine Arias <amandine.arias@gmail.com>
2021-12-07 19:19:12 +0100

    doc (rename) : js to jsx extension

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App/App';
+import './components/App/App.css';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
