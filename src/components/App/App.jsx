b9330d47e1eb2b689948c120095d8fb5293bf1e3
Amandine Arias <amandine.arias@gmail.com>
2021-12-08 15:55:51 +0100

    feat (Routes)

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,21 @@
-import Navbar from '../Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Navbar from '../Navbar/Navbar';
+import Home from '../../pages/Home/Home';
+import Employees from '../../pages/Employees/Employees';
+import Error from '../../pages/Error/Error';
+
 import './App.css';
 
 export default function App() {
   return (
-    <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route exact path="/employees" element={<Employees />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
