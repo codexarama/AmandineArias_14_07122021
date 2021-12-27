96a773f0582dcfad5c4db112827bb83e24e5366d
Amandine Arias <amandine.arias@gmail.com>
2021-12-27 19:08:58 +0100

    feat (jsDoc + PropTypes) / refactor : TableFilter

--- src/components/Table/TableFilter.jsx
+++ src/components/Table/TableFilter.jsx
@@ -1,23 +1,51 @@
 import React, { useState } from 'react';
 import { useAsyncDebounce } from 'react-table';
 
-import search from '../../assets/ico-search.svg';
+import searchIcon from '../../assets/ico-search.svg';
 
-export default function TableFilter({ filter, setFilter }) {
+import PropTypes from 'prop-types';
 
-const [value, setValue] = useState(filter)
+/**
+ * TableFilter
+ *
+ * @param   {object}      props
+ * @param   {string}      props.filter        [initial value]
+ * @param   {function}    props.setFilter     [new value || undefined]
+ * @param   {string}      props.className     [span wrapper className]
+ * @param   {string}      props.id            [label "html for" identifiant]
+ *
+ * @returns {Reactnode}   jsx injected in DOM
+ */
+export default function TableFilter({ filter, setFilter, className, id }) {
+  const [value, setValue] = useState(filter);
 
-const handleChange = useAsyncDebounce(value => {
-    setFilter(value || undefined)
-}, 500)
+  const handleChange = useAsyncDebounce((value) => {
+    setFilter(value || undefined);
+  }, 500);
 
   return (
-    <span className="search">
-      <img src={search} alt="search icon" className='search-icon' /> Search{' '}
-      <input type="text" value={value || ''} onChange={(e) => {
-          setValue(e.target.value)
-          handleChange(e.target.value)
-      }} />
+    <span className={className}>
+      <img src={searchIcon} alt="search icon" />
+      <label htmlFor={id}>Search</label>
+      <input
+        id={id}
+        type="text"
+        value={value || ''}
+        onChange={(e) => {
+          setValue(e.target.value);
+          handleChange(e.target.value);
+        }}
+      />
     </span>
   );
 }
+
+/**
+ * TableFilter PROPTYPES
+ */
+TableFilter.propTypes = {
+  filter: PropTypes.string,
+  setFilter: PropTypes.func.isRequired,
+  className: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+};
