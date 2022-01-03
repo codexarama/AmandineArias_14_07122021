fdd914d0280e15af9dfd9abe333a632a3d22fa9e
Amandine Arias <amandine.arias@gmail.com>
2022-01-03 14:59:57 +0100

    clean code (Home) : remove console.logs

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -11,11 +11,8 @@ export default function Home() {
 
     // FILLING THE ADDRESS BLOCK WITH THE ADDRESS ITEMS
     const addressContainer = document.getElementById('addressContainer');
-    // console.log(addressContainer);
     const addressItems = [...document.getElementsByClassName('address')];
-    // console.log(addressItems);
     addressItems.map((item) => {
-      // console.log(item);
       return <h3>Address</h3> && addressContainer.append(item);
     });
 
@@ -24,39 +21,32 @@ export default function Home() {
     minAge = new Date();
     minAge.setFullYear(minAge.getFullYear() - 68);
     minAge = minAge.toISOString().split('T')[0];
-    // console.log(minAge);
     document.getElementById('dateOfBirth').setAttribute('min', minAge);
 
     let maxAge; // elder than 16 years old
     maxAge = new Date();
     maxAge.setFullYear(maxAge.getFullYear() - 16);
     maxAge = maxAge.toISOString().split('T')[0];
-    // console.log(maxAge);
     document.getElementById('dateOfBirth').setAttribute('max', maxAge);
 
     // HANDLE MIN / MAX DATE FOR START DATE INPUT
     let minStartDate; // first day of the current month
     minStartDate = new Date();
     minStartDate.setMonth(minStartDate.getMonth() - 1);
-    minStartDate = new Date(minStartDate);
     minStartDate = minStartDate.toISOString().split('T')[0];
-    // console.log(minStartDate); // ok : yyyy-mm-dd
     document.getElementById('startDate').setAttribute('min', minStartDate);
 
-    let maxStartDate // current day
+    let maxStartDate; // current day
     maxStartDate = new Date();
     maxStartDate.setDate(maxStartDate.getDate());
     maxStartDate = maxStartDate.toISOString().split('T')[0];
-    // console.log(maxStartDate); // ok : yyyy-mm-dd
     document.getElementById('startDate').setAttribute('max', maxStartDate);
   });
 
   return (
-    <main className="add-employee">
-      <section className="add-employee-content">
-        <h2>Add an employee</h2>
-        <Form />
-      </section>
+    <main aria-labelledby="title">
+      <h2 id="title">Add an employee</h2>
+      <Form />
     </main>
   );
 }
