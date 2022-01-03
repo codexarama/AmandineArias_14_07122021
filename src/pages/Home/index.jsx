f750e9614970dcdf3fc040fc31e4d8dd49add1e2
Amandine Arias <amandine.arias@gmail.com>
2022-01-03 14:35:14 +0100

    feat (Form) : handle min/max values dot date type inputs

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -16,11 +16,39 @@ export default function Home() {
     // console.log(addressItems);
     addressItems.map((item) => {
       // console.log(item);
-      return (
-        <h3>Address</h3> &&
-        addressContainer.append(item)
-        );
+      return <h3>Address</h3> && addressContainer.append(item);
     });
+
+    // HANDLE MIN / MAX AGE FOR DATE OF BIRTH INPUT
+    let minAge; // younger than 68 years old
+    minAge = new Date();
+    minAge.setFullYear(minAge.getFullYear() - 68);
+    minAge = minAge.toISOString().split('T')[0];
+    // console.log(minAge);
+    document.getElementById('dateOfBirth').setAttribute('min', minAge);
+
+    let maxAge; // elder than 16 years old
+    maxAge = new Date();
+    maxAge.setFullYear(maxAge.getFullYear() - 16);
+    maxAge = maxAge.toISOString().split('T')[0];
+    // console.log(maxAge);
+    document.getElementById('dateOfBirth').setAttribute('max', maxAge);
+
+    // HANDLE MIN / MAX DATE FOR START DATE INPUT
+    let minStartDate; // first day of the current month
+    minStartDate = new Date();
+    minStartDate.setMonth(minStartDate.getMonth() - 1);
+    minStartDate = new Date(minStartDate);
+    minStartDate = minStartDate.toISOString().split('T')[0];
+    // console.log(minStartDate); // ok : yyyy-mm-dd
+    document.getElementById('startDate').setAttribute('min', minStartDate);
+
+    let maxStartDate // current day
+    maxStartDate = new Date();
+    maxStartDate.setDate(maxStartDate.getDate());
+    maxStartDate = maxStartDate.toISOString().split('T')[0];
+    // console.log(maxStartDate); // ok : yyyy-mm-dd
+    document.getElementById('startDate').setAttribute('max', maxStartDate);
   });
 
   return (
