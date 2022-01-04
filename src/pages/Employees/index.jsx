eae6a735b68751cb07a784c483ebe3171c2649b3
Amandine Arias <amandine.arias@gmail.com>
2022-01-04 19:57:10 +0100

    feat (accessibility) : ARIA convention => scope attributes for nested headers

--- src/pages/Employees/index.jsx
+++ src/pages/Employees/index.jsx
@@ -1,4 +1,8 @@
 import { useEffect } from 'react';
+
+import { getTags } from '../../utils/handlers';
+import { setAttributes } from '../../utils/handlers';
+
 import Table from '../../components/Table';
 
 /**
@@ -8,11 +12,28 @@ import Table from '../../components/Table';
 export default function Employees() {
   useEffect(() => {
     document.title = 'HRnet | Employees';
+
+    // ACCESSIBILITY
+    // HANDLING HEADERS SCOPE BY SETTING ATTRIBUTE
+    // headers level 1
+    getTags('tr', 0, 'th').map((item) =>
+      setAttributes(item, {
+        scope: "colgroup",
+      })
+    );
+    // headers level 2
+    getTags('tr', 1, 'th').map((item) =>
+      setAttributes(item, {
+        scope: "col",
+      })
+    );
   });
 
   return (
     <main aria-labelledby="page-title" className="table">
-      <h2 tabIndex="0" id="page-title">Employees list</h2>
+      <h2 tabIndex="0" id="page-title">
+        Employees list
+      </h2>
       <Table />
     </main>
   );
